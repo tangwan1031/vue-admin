fca6f2a10ba0285dde227036863abd96cac36463
tangwan <leelhef@126.com>
2021-02-08 22:02:33 +0800

    hhhh

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page lalalalala</h1>
-    <div>婉儿最棒的 加油！fight</div>
+    <h1>This is an about page lalalalalajkkkkkkkjjlkjkjfksdfjkdfdjfhjh</h1>
+    <div>婉儿最棒的 加油！fight hjjljkljkhggjjhhhhnjggjhgjhgjh</div>
   </div>
 </template>
